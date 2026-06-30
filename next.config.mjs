9222fd64a8c4f8cc6b7abf81a3f25874d1d0b97f
Miguel Cruz <miguelcruz@Miguels-MacBook-Pro.local>
2026-06-30 17:08:40 -0400

    fix: redirect /privacy to /privacy-policy

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/privacy',
+        destination: '/privacy-policy',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
+
